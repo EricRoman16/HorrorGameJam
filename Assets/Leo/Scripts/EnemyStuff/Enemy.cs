8236871df4c77e606625c5694a18912a25815239
Leo <leo_1517@outlook.com>
2022-06-16 15:56:38 -0700

    Imported A* and added basic scripts for enemy and rooms

---
+++ Assets/Leo/Scripts/EnemyStuff/Enemy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy
+{
+    public bool isChasing;
+    public bool hasDirectSight;
+    public float ecounterProgress;
+    public float evadeProgress;
+}
---
+++ Assets/Leo/Scripts/RoomStuff/Room.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Room
+{
+    public int roomID;
+    public string roomName;
+    public RoomType roomType;
+    public Exit[] exitCount;
+}
+
+public enum RoomType
+{
+    normal,
+    scripted,
+    safe
+}
