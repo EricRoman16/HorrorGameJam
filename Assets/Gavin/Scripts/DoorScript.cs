d4c7d2633d821ad1dfb9cc06bc5aac2d2d4e0756
1707344 <gavinjohnson2004@gmail.com>
2022-06-19 19:02:48 -0700

    Pushing for leo

---
+++ Assets/Gavin/Scripts/DoorScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class DoorScript : MonoBehaviour
+{
+    public RoomScript[] connectedRooms;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void UpdateRooms()
+    {
+        if(connectedRooms == null || connectedRooms.Length < 2)
+        {
+            connectedRooms = new RoomScript[2];
+        }
+        
+        if(connectedRooms[0] == null)
+        {
+
+            connectedRooms[0] = GetClosestRoom(connectedRooms[1]);
+        }
+        
+        if(connectedRooms[1] == null)
+        {
+            connectedRooms[1] = GetClosestRoom(connectedRooms[0]);
+        }
+    }
+    RoomScript GetClosestRoom(RoomScript ignoreRoom)
+    {
+        RoomScript[] rooms = transform.parent.GetComponentsInChildren<RoomScript>();
+        if(rooms.Length == 0)
+        {
+            return null;
+        }
+
+        RoomScript closestRoom = rooms[0] == ignoreRoom ? rooms[1] : rooms[0];
+        float dist = Vector2.Distance(closestRoom.transform.position, transform.position);
+        for (int i = 0; i < rooms.Length; i++)
+        {
+            RoomScript newRoom = rooms[i];
+            if(newRoom == ignoreRoom)
+            {
+                continue;
+            }
+            float newDist = Vector2.Distance(newRoom.transform.position, transform.position);
+            if (newDist < dist)
+            {
+                dist = newDist;
+                closestRoom = newRoom;
+            }
+        }
+        return closestRoom;
+    }
+}
+
---
+++ Assets/Gavin/Scripts/RoomScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomScript : MonoBehaviour
+{
+    public BoxCollider2D boxCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
