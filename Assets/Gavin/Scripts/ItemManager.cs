4404e84651911a6ad04d26a3f571213ab9d7cc3a
1707344 <gavinjohnson2004@gmail.com>
2022-08-31 23:25:00 -0700

    Item Selection

--- Assets/Gavin/Scripts/ItemManager.cs
+++ Assets/Gavin/Scripts/ItemManager.cs
@@ -2,24 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class ItemManager : MonoBehaviour
 {
     TextMeshProUGUI itemDesc;
     TextMeshProUGUI itemName;
 
+    [System.Serializable]
+    public class ItemInfo
+    {
+        public string name;
+        public string description;
+        public Sprite icon;
+    }
+
+    [SerializeField]
+    ItemInfo[] itemInfo;
+
+    List<ItemInfo> acquiredItems;
+
+    GameObject[] itemSlots;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        acquiredItems = new List<ItemInfo>();
+        
+        itemSlots = GameObject.FindGameObjectsWithTag("ItemSlot");
+
+        foreach (ItemInfo item in itemInfo)
+        {
+            acquiredItems.Add(item);
+        }
+        print(itemSlots.Length);
+        print(acquiredItems.Count);
+
         GameObject itemDescParent = GameObject.Find("ItemDescription");
         itemName = itemDescParent.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
         itemDesc = itemDescParent.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+
+        UpdateInventory();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void UpdateInventory()
+    {
+        for (int i = 0; i < acquiredItems.Count; i++)
+        {
+            if(i >= itemSlots.Length)
+            {
+                break;
+            }
+            itemSlots[i].GetComponent<Image>().sprite = acquiredItems[i].icon;
+            itemSlots[i].GetComponent<ItemData>().itemName = acquiredItems[i].name;
+            itemSlots[i].GetComponent<ItemData>().itemDescription = acquiredItems[i].description;
+        }
     }
 
     public void ShowItemData(ItemData data)
